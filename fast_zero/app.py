09bf90a1f5f1368f0234992500a20851a881ff0f
Otávio Augusto Portela Luciano <otavio.luciano@dgranel.com.br>
2025-08-31 12:55:28 -0300

    Aula 3

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Ola Mundo!'}
+
+
+@app.get('/exe2', response_class=HTMLResponse)
+def exe2():
+    return """
+    <html>
+        <head>
+            <title>Exercicio 2</title>
+        </head>
+        <body>
+            <h1>Ola Mundo!</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,13 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Ola Mundo!'}
+
+
+def test_exe2_deve_retornar_html_com_ola_mundo():
+    """Test method get"""
+    client = TestClient(app)
+
+    response = client.get('/exe2')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Ola Mundo!</h1>' in response.text
